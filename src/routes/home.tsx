694471fd58d238e6439aa179b2d442b9d6a2181c
jissssoo1717 <jisoolee7101@gmail.com>
2023-12-26 14:01:17 +0900

    Fixing Home tap css

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,23 +1,24 @@
 import styled from "styled-components";
 
 const Container = styled.div`
+  position: relative;
   width: 100%;
   height: 100vh;
   display: flex;
 `;
 
 const Tap = styled.div`
+  position: absolute;
   width: 80%;
   height: 100%;
+  top: 0;
+  right: 0;
 `;
 
 export const Home = () => {
   return (
     <Container>
-      {/* 네비게이션바 */}
-
-      {/* 탭창(메뉴 아이콘 클릭 시 내용 표시) */}
-      <Tap></Tap>
+      <Tap>Home</Tap>
     </Container>
   );
 };
